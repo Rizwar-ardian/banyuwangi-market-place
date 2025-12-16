6e53823542e00fdbb8e5dc59e33308a256506e2b
Rizwar-ardian <ardianrizwar486@gmail.com>
2025-12-16 17:14:21 +0700

    Vendor B

--- Api.js
+++ Api.js
@@ -1,15 +1,16 @@
-const http = require("http");
+const express = require("express");
+const router = express.Router();
+const vendorAController = require("../controllers/vendorAControllers");
+const vendorBController = require("../controllers/vendorBControllers");
+const vendorCController = require("../controllers/vendorCController");
 
-http
-  .get("http://localhost:3000/api/vendor-c", (res) => {
-    let data = "";
-    res.on("data", (chunk) => {
-      data += chunk;
-    });
-    res.on("end", () => {
-      console.log(data);
-    });
-  })
-  .on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
+// Mendefinisikan route untuk mendapatkan data Vendor A
+router.get("/vendor-a", vendorAController.getVendorA);
+
+// Mendefinisikan route untuk mendapatkan data Vendor B
+router.get("/vendor-b", vendorBController.getVendorB);
+
+// Mendefinisikan route untuk mendapatkan data Vendor C
+router.get("/vendor-c", vendorCController.getVendorC);
+
+module.exports = router;
